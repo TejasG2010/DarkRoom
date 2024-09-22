2814d2e9be55f660551c6f51da7515135da938ad
exoti <sidk94021@gmail.com>
2024-09-22 09:40:10 -0500

    Second Day Started

---
+++ src/SecondDay.java
@@ -0,0 +1,34 @@
+
+public class SecondDay {
+
+    public static void playSecondDay() {
+        Util.displayMessageWithEnter("*Mysterious Voice* You have survived the night, Hmm... Suprising");
+        Util.displayMessageWithEnter("*Mysterious Voice* Now you must escape, Happy Journey 'Chosen One'");
+        Util.displayMessageWithEnter("*Bai* Hi, nice day today right, your energy levels have reset to 100% and you have an axe, some food and //fillHere ");
+        Util.displayMessageWithEnter("Today is your last day to stay here or you will become one of us!!!");
+        Util.displayMessageWithEnter("*You* 'ONE OF YOU!!!'");
+        Util.displayMessageWithEnter("*Bai* Oh! Master didn't tell you");
+        Util.displayMessageWithEnter("*You* MASTER! WHAT DO YOU MEAN??!!");
+        Util.displayMessageWithEnter("*Bai* Chosen One, you really don't know let me run your memory");
+        Util.displayMessageWithEnter("*Flashback* You are fortunate to be the “Chosen One”...");
+        Util.displayMessageWithEnter("*Flashback* Soon you will understand..");
+        Util.displayMessageWithEnter("*Flashback* Sooner than expected...");
+        Util.displayMessageWithEnter("The young white dog turns into a white ox, with a horned head and a dragon body, has 9 eyes *BAI* 'I am BAI ZE, the warder of evil spirits and 'THE WHITE MARSH' ");
+        Util.displayMessageWithEnter("*Bai* I am your demon animal, there are many such as Chimeras, and Hydras, I am one among them");
+        Util.displayMessageWithEnter("The Abysmal Chasm not only connects the IRL to our world, it has various dangerous demons to attack any trespassers");
+        Util.displayMessageWithEnter("As you are shocked by this discovery, you realize in order to prevent changing into a demon you need to go home");
+        Util.displayMessageWithEnter("*You* Bai I don't know how I got here so I don't know where to go!!!");
+        Util.displayMessageWithEnter("You look down in frustration and see a drawing edged on the surface of the dirty wood");
+        Util.displayMessageWithEnter("You wipe all the dust and see that it is the  ");
+        Util.displayMessageWithEnter("You are fortunate to be the “Chosen One”...");
+        Util.displayMessageWithEnter("You are fortunate to be the “Chosen One”..");
+        Util.displayMessageWithEnter("You are fortunate to be the “Chosen One”...");
+        Util.displayMessageWithEnter("You are fortunate to be the “Chosen One”..");
+        Util.displayMessageWithEnter("You are fortunate to be the “Chosen One”...");
+        Util.displayMessageWithEnter("You are fortunate to be the “Chosen One”..");
+        Util.displayMessageWithEnter("You are fortunate to be the “Chosen One”...");
+        Util.displayMessageWithEnter("You are fortunate to be the “Chosen One”..");
+        Util.displayMessageWithEnter("You are fortunate to be the “Chosen One”...");
+
+    }
+}
